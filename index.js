fd91dcde8acf09b46394c5c801768e146a97307b
Nick Scofield <c.nick.scofield@gmail.com>
2022-12-14 23:51:26 -0700

    commit tree 30d16914e90fd64583bdc327a0785710bba1ba73

--- index.js
+++ index.js
@@ -12,5 +12,11 @@ const tutorials = [
 ];
 
 const titleCased = () => {
-  return tutorials
+    return tutorials.map(tut => {
+      const words = tut.split(" ");
+      const newWords = words.map(word => `${word.charAt(0).toUpperCase()}${word.slice(1)}`)
+      return newWords.join(" ");
+    })
 }
+
+titleCased();
